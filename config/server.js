def4051e35d9455a1749594fd3efd3c82bec534c
Kristaps <krim@html24.net>
2021-11-12 23:51:45 +0100

    badd beers added

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c9bc13fc4edfbb2978140aa18335759'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '300bb49c-6468-4eab-8a58-348fc173a64b'
+};
